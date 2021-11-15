b9df27b479c55bf467e07b201c53ce8ff2f50435
Luke <74873871+LucasCampardo@users.noreply.github.com>
2021-11-15 11:15:59 -0300

    Delete precendecia.py

--- precendecia.py
+++
@@ -1,4 +0,0 @@
-# Precedência 0 = (.......) OBS: Independente de qual operação estiver dentro do parenteses, sempre terá prioridade
-# Precedência 1 = ** (Potência)
-# Precedência 2 = *, /, //, % (Multiplicação, Divisão, Divisão Inteira, Módulo)
-# Precedência 3 = +, - (Adição, Subtração)
